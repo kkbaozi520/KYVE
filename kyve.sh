1c6ad7b3ce461e6dc77df797b365a650529beeb0
kkbaozi520 <95422814+kkbaozi520@users.noreply.github.com>
2022-06-19 21:56:25 +0800

    Update kyve.sh

--- kyve.sh
+++ kyve.sh
@@ -34,9 +34,11 @@ $mnemonic
 EOF
 mv mnemonic.txt $HOME/kysor/secrets
 sudo mv /home/ubuntu/arweave.json $HOME/kysor/secrets
+sudo mv restart.sh $HOME/kysor
 sed -i.bak -e "s/initialStake: 100/initialStake: $amount/; s/poolId: 0/poolId: $pooid/" $HOME/kysor/kysor.conf.ts
 cd kysor
 yarn install
 yarn build
-yarn start
+nohup sh restart.sh >> restart.out &
+ls
 echo -e '\n\e[42mInstallation completed\e[0m\n'
